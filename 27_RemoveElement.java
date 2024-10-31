f3c16707fc2d22f8025ccab1b390916230707abf
Joseph Dubill <32132614+joedubill@users.noreply.github.com>
2024-10-31 10:36:01 -0400

    Added solution description

--- 27_RemoveElement.java
+++ 27_RemoveElement.java
@@ -12,17 +12,15 @@ class Solution {
 }
 
 
-//
-27. Remove Element
-Solved
-Easy
-Topics
-Companies
-Hint
-Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-
-Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-
-Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
-Return k.
-//
+/*
+ *   27. Remove Element - Easy
+ *
+ *   Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ *
+ *   Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+ *   Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ *   Return k.
+ *
+ *   Solution: Two pointers. Iterate though nums with one pointer to check if we are on an element equal to val, and one pointer to set elements not equal to val, to overwrite target elements with on target elements. 
+ *   
+ */
